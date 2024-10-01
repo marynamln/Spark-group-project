2f08e54da59ad567f7eb137fca832c82c787c86a
ivannamal <imalaschuk@gmail.com>
2024-10-01 15:59:52 +0300

    added pyspark session

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
+import pyspark
+from pyspark import SparkConf
+from pyspark.sql import SparkSession
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print('This is the project of Melnyk, Malashchuk and Minduk')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+spark_session = (SparkSession.builder
+                             .master("local")
+                             .appName("task app")
+                             .config(conf=SparkConf())
+                             .getOrCreate())
