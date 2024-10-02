b8f7d546432ca45d195771a1c5d2189600cc9369
Victoriia Mindiuk <v.mindiuk@ukma.edu.ua>
2024-10-02 14:22:14 +0300

    added .gitignore, main.py and Dockerfile

--- main.py
+++ main.py
@@ -1,11 +1,1 @@
-import pyspark
-from pyspark import SparkConf
-from pyspark.sql import SparkSession
-if __name__ == '__main__':
-    print('This is the project of Melnyk, Malashchuk and Minduk')
-
-spark_session = (SparkSession.builder
-                             .master("local")
-                             .appName("task app")
-                             .config(conf=SparkConf())
-                             .getOrCreate())
+print("This is the project of Mindiuk, Melnuk and Malashchuk")
