328a441f692bcb6d535702c16ac882fc91c0a9c0
Victoriia Mindiuk <v.mindiuk@ukma.edu.ua>
2024-10-14 00:50:38 +0300

    created basic_test_df function

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import pyspark
 from pyspark import SparkConf
 from pyspark.sql import SparkSession
-if __name__ == '__main__':
-    print('This is the project of Melnyk, Malashchuk and Minduk')
+from basic_dfc.basic_df_mindiuk import basic_test_df
 
-spark_session = (SparkSession.builder
-                             .master("local")
-                             .appName("task app")
-                             .config(conf=SparkConf())
-                             .getOrCreate())
+def display_test_df():
+    spark_session = (SparkSession.builder
+                     .master("local")
+                     .appName("task app")
+                     .config(conf=SparkConf())
+                     .getOrCreate())
+
+    df = basic_test_df()
+    df.show()
+
+if __name__ == '__main__':
+    print('This is the project of Melnyk, Malashchuk and Mindiuk')
+    display_test_df()
