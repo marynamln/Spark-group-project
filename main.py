23545bfef0a25afe5ed9eb5a340164f547d9e5ea
Maryna Melnyk <melnikm535@gmail.com>
2024-10-15 23:25:46 +0300

    update write.csv function

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ from input_output import read_imdb_name_basics_df, write_imdb_name_basics_df_to_
 
 if __name__ == '__main__':
     df = read_imdb_name_basics_df()
-    write_imdb_name_basics_df_to_csv(df)
+    write_imdb_name_basics_df_to_csv(df, output_path="data/results/name.csv", num_rows=20, mode="overwrite")
