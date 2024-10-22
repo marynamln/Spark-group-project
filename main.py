0196652265930813ee9000eaffca351fb57230e4
Maryna Melnyk <melnikm535@gmail.com>
2024-10-22 14:26:37 +0300

    add fill_null_value_df function

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from basic_dfc.basic_df_malashchuk import basic_test_df as mal
 from basic_dfc.basic_df_mindiuk import basic_test_df
 
 from input_output import read_imdb_name_basics_df, write_imdb_name_basics_df_to_csv
-from name_df_cleaning_and_transformation import convert_columns_to_snake_case, convert_death_year_to_int
+from name_df_cleaning_and_transformation import (convert_columns_to_snake_case,
+                                                 convert_death_year_to_int,
+                                                 fill_missing_values)
 
 if __name__ == '__main__':
     df = read_imdb_name_basics_df()
@@ -12,7 +14,8 @@ if __name__ == '__main__':
 
     renamed_col_df = convert_columns_to_snake_case(df)
     transformed_dy_col_df = convert_death_year_to_int(renamed_col_df)
-    transformed_dy_col_df.show(50)
+    fill_null_value_df = fill_missing_values(transformed_dy_col_df)
+    fill_null_value_df.show(60)
 
-    transformed_dy_col_df.printSchema()
-
+    fill_null_value_df.filter(fill_null_value_df["death_year"].isNull()).show(50)
+    fill_null_value_df.filter(fill_null_value_df["birth_year"].isNull()).show(50)
--- name_df_cleaning_and_transformation.py
+++ name_df_cleaning_and_transformation.py
@@ -13,3 +13,9 @@ def convert_columns_to_snake_case(df):
 
 def convert_death_year_to_int(df):
     return df.withColumn("death_year", df["death_year"].cast("int"))
+
+def fill_missing_values(df):
+    df = df.withColumn("death_year", f.when(df["death_year"].isNull(), 0).otherwise(df["death_year"]))
+    df = df.withColumn("birth_year", f.when(df["birth_year"].isNull(), 0).otherwise(df["birth_year"]))
+
+    return df
